64c01bd36a3cae55574fc7f379c98031560af084
limangbai <2855682925@qq.com>
2019-04-11 08:41:10 +0800

    Pythoncharm 上传

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,17 @@
 magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
-    print(magician.title())
+    print(magician.title() + ",that was a grest trick!")
+    print("I can't wait to see your next tridk, " + magician.title() + '.\n')
+print("Thank you,everyone.That was a great magic show!")
+
+#pizza
+pizzas = ['pizzas1', 'pizzas2', 'pizzas3']
+for pizza in pizzas:
+    print('I like ' + pizza.title())
+print('I really love pizza')
+
+#pet
+pets = ["dog", "cat", "pig", "tiger"]
+for pet in pets:
+    print("A " + pet + " would make a great pet!")
+print('Any of these animals would make a great pet!')
